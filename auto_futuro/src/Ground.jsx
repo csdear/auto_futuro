4031817ec8533818f788cc59cfc7daba959962de
cdear <cdear@ebsco.com>
2024-03-13 08:54:38 -0500

    groundwork

---
+++ auto_futuro/src/Ground.jsx
@@ -0,0 +1,46 @@
+import { MeshReflectorMaterial } from "@react-three/drei";
+import { useLoader } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
+import { BufferAttribute } from "three";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+
+import grid from "../src/textures/grid.png";
+import groundAO from "../src/textures/ground-ao.png";
+import alpha from "../src/textures/alpha-map.png";
+
+export function Ground() {
+    const gridMap = useLoader(
+        TextureLoader,
+        process.env.PUBLIC_URL + grid
+    );
+
+    const aoMap = useLoader(
+        TextureLoader,
+        process.env.PUBLIC_URL + groundAO
+    );
+
+    const alphaMap = useLoader(
+        TextureLoader,
+        process.env.PUBLIC_URL + alpha
+    );
+
+    useEffect(() => {
+        gridMap.anisotropy = 16;
+    }, [gridMap]);
+
+    return (
+        <>
+        {*/ standard mesh, applying a GEOMETRY and a MATERIAL. */}
+        <mesh
+            ref={meshRef}
+            position={[-2.285, -0.015, -1.325]}
+            rotation-x={-Math.PI * 0.5}
+            rotation-z={-0.079}
+        >
+        <circleGeometry args={[6.12, 50]} />
+        <MeshReflectorMaterial />
+
+        </mesh>
+        </>
+    );
+}
