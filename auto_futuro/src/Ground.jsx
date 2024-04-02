fc6e1e0182974af77acf2a3f9c07f6d8c564a524
Stuart Dear <stuart.dear@intermarkgroup.com>
2024-04-02 13:08:09 -0500

    add the white grid

--- auto_futuro/src/Ground.jsx
+++ auto_futuro/src/Ground.jsx
@@ -29,13 +29,32 @@ export function Ground() {
     }, [gridMap]);
 
     const meshRef = useRef(null);
+    const meshRef2 = useRef(null);
+
     useEffect(() => {
         var uvs = meshRef.current.geometry.attributes.uv.array;
         meshRef.current.geometry.setAttribute("uv2", new BufferAttribute(uvs, 2));
-    }, [meshRef.current]);
+
+        var uvs2 = meshRef2.current.geometry.attributes.uv.array;
+        meshRef2.current.geometry.setAttribute("uv2", new BufferAttribute(uvs2, 2));
+        }, [meshRef.current]);
 
     return (
         <>
+        <mesh
+        ref={meshRef2}
+        position={[-2.285, -0.01, -1.325]}
+        rotation-x={-Math.PI * 0.5}
+        >
+            <planeGeometry args={[12, 12]} />
+            <meshBasicMaterial
+            opacity={0.325}
+            alphaMap={gridMap}
+            transparent={true}
+            color={"white"}
+            />
+        </mesh>
+
         <mesh
             ref={meshRef}
             position={[-2.285, -0.015, -1.325]}
