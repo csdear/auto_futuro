b82914219a4c7142ee3f5425249c8f81f3b33861
Stuart Dear <stuart.dear@intermarkgroup.com>
2024-04-03 14:50:09 -0500

    adding cannon physics engine for the car physics entity. Demonstrates the fall through effect

--- auto_futuro/src/Car.jsx
+++ auto_futuro/src/Car.jsx
@@ -1,3 +1,4 @@
+import { useBox } from "@react-three/cannon";
 import { useFrame, useLoader } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
@@ -11,12 +12,44 @@ export function Car() {
         process.env.PUBLIC_URL + carGLB
     ).scene;
 
+    const position = [-1.5, 0.5, 3];
+    const width = 0.15;
+    const height = 0.07;
+    const front = 0.15;
+    const wheelRadius = 0.05;
+
+    const chassisBodyArgs = [width, height, front * 2];
+    const [chassisBody, chassisApi] = useBox(
+    () => ({
+        allowSleep: false,
+        args: chassisBodyArgs,
+        mass: 150,
+        position,
+    }),
+    useRef(null),
+    );
+
+    // const [wheels, wheelInfos] = useWheels(width, height, front, wheelRadius);
+    // const [vehicle, vehicleApi] = useRaycastVehicle(
+    //     () => ({
+    //         chassisBody,
+    //         wheelInfos,
+    //         wheels,
+    //     }),
+    //     useRef(null),
+    //     );
+
+
     useEffect(() => {
         mesh.scale.set(0.0012, 0.0012, 0.0012 );
         mesh.children[0].position.set(-365, -18, -67);
     }, [mesh]);
 
     return (
-        <primitive object={mesh} rotation-y={Math.PI} />
+        // <primitive object={mesh} rotation-y={Math.PI} />
+        <mesh ref={chassisBody}>
+            <meshBasicMaterial transparent={true} opacity={0.3} />
+            <boxGeometry args={chassisBodyArgs} />
+        </mesh>
     );
 }
